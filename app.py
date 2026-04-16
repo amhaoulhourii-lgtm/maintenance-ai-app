d5f89a0d881fa518d4868feb05ba8eb476150b98
amhaoulhourii-lgtm <amhaoulhourii@gmail.com>
2026-04-16 19:21:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import streamlit as st
 import pandas as pd
 
-st.title("Application Maintenance IA")
+st.title("Maintenance des applications IA")
 
-file = st.file_uploader("Upload ton fichier Excel", type=["xlsx"])
+file = st.file_uploader("Téléversez votre fichier Excel", type=["xlsx"])
 
 if file:
     df = pd.read_excel(file)
